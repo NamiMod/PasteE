c6fa74ebece7701f052f21aa6526162ef79d91f8
Nami Modarressi <nami@Namis-MacBook-Pro.local>
2022-10-14 10:12:49 +0330

    base

---
+++ PasteE/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PasteE
+//
+//  Created by Nami Modarressi on 7/22/1401 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PasteE/PasteEApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasteEApp.swift
+//  PasteE
+//
+//  Created by Nami Modarressi on 7/22/1401 AP.
+//
+
+import SwiftUI
+
+@main
+struct PasteEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
